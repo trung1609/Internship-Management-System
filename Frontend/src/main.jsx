d031c7247518469f2667444ecff951a1cbf86720
trung1609 <vmt162005@gmail.com>
2026-06-09 12:02:49 +0700

    Change BrowserRouter to HashRouter for GitHub Pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { AuthProvider } from './context/AuthContext.jsx';
 
@@ -15,13 +15,14 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* 2. Đổi thẻ bọc ngoài cùng thành HashRouter */}
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline /> 
         <AuthProvider>
             <App />
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
